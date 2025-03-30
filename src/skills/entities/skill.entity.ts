accbd5aee31742579260cd68b652f19afa68ed9e
Sergio Roman <sergio.romam@outlook.com>
2025-03-30 14:11:03 -0600

    delete innecessary imports

--- src/skills/entities/skill.entity.ts
+++ src/skills/entities/skill.entity.ts
@@ -1,20 +1,18 @@
-// import { JobOffer } from "src/job-offers/entities/job-offer.entity";
-import { Candidate } from "src/candidate-details/entities/candidate.entity";
-import { Offer } from "src/offers/entities/offer.entity";
-// import { Offer } from "src/offers/entities/offer.entity";
-import { Entity, Column, PrimaryGeneratedColumn, ManyToMany } from "typeorm";
+import { Candidate } from 'src/candidate-details/entities/candidate.entity';
+import { Offer } from 'src/offers/entities/offer.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany } from 'typeorm';
 
 @Entity('skills')
 export class Skill {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({ type: 'text', unique: true })
-    name: string;
+  @Column({ type: 'text', unique: true })
+  name: string;
 
-    @ManyToMany(() => Candidate, candidate => candidate.skills)
-    candidates: Candidate[];
+  @ManyToMany(() => Candidate, (candidate) => candidate.skills)
+  candidates: Candidate[];
 
-    @ManyToMany(() => Offer, offer => offer.requiredSkills)
-    offers: Offer[];
+  @ManyToMany(() => Offer, (offer) => offer.requiredSkills)
+  offers: Offer[];
 }
